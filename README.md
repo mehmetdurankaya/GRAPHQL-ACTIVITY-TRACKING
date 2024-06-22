9443b123f1f9b161f13bca1f878a73c5c61fe29f
mehmetdurankaya <mehmetdurankaya@gmai.com>
2024-06-22 14:27:28 +0300

    CRUD operation

--- README.md
+++ README.md
@@ -6,6 +6,45 @@
 ### Bir Event, bir User ile ilişkilidir.
 ### Bir Event, bir Location ile ilişkilidir.
 ### Bir Event birden fazla Participant ile ilişkilidir.
+# User
+### User ekleyecek Mutation yazıldı.
+### User'ı güncelleyecek olan Mutation yazıldı.
+### Bir User'ı silecek olan Mutation yazıldı.
+### Tüm User'ları silecek olan Mutation yazıldı.
+    * mutation addUser 
+    * mutation updateUser
+    * mutation deleteUser
+    * mutation deleteAllUsers
+
+# Event
+### Event ekleyecek Mutation yazıldı.
+### Event'i güncelleyecek olan Mutation yazıldı.
+### Bir Event'i silecek olan Mutation yazıldı.
+### Tüm Event'leri silecek olan Mutation yazıldı.
+    * mutation addEvent 
+    * mutation updateEvent
+    * mutation deleteEvent
+    * mutation deleteAllEvents
+
+# Location
+### Location ekleyecek Mutation yazıldı.
+### Location'ı güncelleyecek olan Mutation yazıldı.
+### Bir Location'ı silecek olan Mutation yazıldı.
+### Tüm Location'ları silecek olan Mutation yazıldı.
+    * mutation addLocation 
+    * mutation updateLocation
+    * mutation deleteLocation
+    * mutation deleteAllLocations
+# Participant
+### Participant ekleyecek Mutation yazıldı.
+### Participant'ı güncelleyecek olan Mutation yazıldı.
+### Bir Participant'ı silecek olan Mutation yazıldı.
+### Tüm Participant'ları silecek olan Mutation yazıldı.
+    * mutation addParticipant 
+    * mutation updateParticipant
+    * mutation deleteParticipant
+    * mutation deleteAllParticipants
+
   ![alt text](1.jpg)
 
 ![alt text](2.jpg)
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { ApolloServer, gql } = require("apollo-server");
 const { users, events, participants, locations } = require("./data.json");
+const {nanoid}=require('nanoid');
 const {
   ApolloServerPluginLandingPageGraphQLPlayground,
 } = require("apollo-server-core");
 
-const typeDefs = gql`
+const typeDefs = gql` 
   #User
   type User {
     id: ID!
@@ -13,6 +14,16 @@ const typeDefs = gql`
     events: [Event!]!
     event(id: ID): Event!
   }
+    input createUserInput{
+      username:String!
+      email:String!
+    }
+
+
+    input updateUserInput{
+      username:String
+      email:String
+    }
 
   #Event
   type Event {
@@ -28,6 +39,26 @@ const typeDefs = gql`
     participants: [Participant!]!
     location: Location!
   }
+  input createEventInput{
+    title:String!
+    desc: String!
+    date: String!
+    from: String
+    to: String
+    location_id:ID
+    user_id:ID
+  }
+
+ input updateEventInput{
+    title: String
+    desc: String
+    date: String
+    from: String
+    to: String
+    location_id: ID
+    user_id: ID
+ }
+
   #Location
   type Location {
     id: ID!
@@ -36,6 +67,21 @@ const typeDefs = gql`
     lat: Float!
     lng: Float!
   }
+  input createLocationInput{
+    name:String!
+    desc:String!
+    lat:Float!    
+    lng:Float!
+  }
+  input updateLocationInput{
+    id: ID
+    name: String
+    desc: String
+    lat: Float
+    lng: Float
+  }
+
+
   #Participant
   type Participant {
     id: ID!
@@ -43,6 +89,21 @@ const typeDefs = gql`
     event_id: ID!
     user: User!
     event: Event!
+  }
+
+  input createParticipantInput{
+    user_id:ID!
+    event_id:ID!
+  }
+
+  input updateParticipantInput{
+    id: ID
+    user_id: ID
+    event_id: ID    
+  }
+
+  type DeleteAllOutput{
+  count:Int!
   }
 
   type Query {
@@ -63,9 +124,200 @@ const typeDefs = gql`
     participants: [Participant!]!
     participant(id: ID!): Participant
   }
+ type Mutation{
+ # User
+  addUser(data:createUserInput):User!
+  updateUser(id: ID!,data:updateUserInput!):User!
+  deleteUser(id:ID!):User!
+  deleteAllUsers:DeleteAllOutput!
+
+  #Event
+  addEvent(data:createEventInput):Event!
+  updateEvent(id:ID!, data:updateEventInput!):Event!
+  deleteEvent(id:ID!):Event!
+  deleteAllEvents:DeleteAllOutput!
+
+  #Location
+  addLocation(data:createLocationInput):Location!
+  updateLocation(id:ID!,data:updateLocationInput!):Location!
+  deleteLocation(id:ID!):Location!
+  deleteAllLocations:DeleteAllOutput!
+
+
+  #Participant
+  addParticipant(data:createParticipantInput):Participant!  
+  updateParticipant(id:ID!,data:updateParticipantInput!):Participant!
+  deleteParticipant(id:ID!):Participant!
+  deleteAllParticipants:DeleteAllOutput!
+  }
+  
 `;
 
 const resolvers = {
+  Mutation:{
+    //User    
+    addUser:(parent,{data})=>{
+      const user={
+        id:nanoid(),
+        ...data
+      }
+     users.push(user);
+     return user;
+    },
+    updateUser:(parent,{id,data})=>{
+         
+        const user_index = users.findIndex((user) => user.id === parseInt(id)); 
+          if(user_index === -1){         
+          throw new Error("User not found");
+        }
+        const updated_user=users[user_index]={
+          ...users[user_index],
+          ...data,
+        }
+
+
+        return updated_user;
+    },
+    deleteUser:(parent,{id})=>{
+      const numId=parseInt(id);
+      const user_index=users.findIndex((user)=>user.id===numId);
+      if(user_index===-1){
+        throw new Error("User not found");
+      }
+      const deleted_user=users[user_index];
+      users.splice(user_index, 1);
+      return deleted_user;
+    },
+    deleteAllUsers:()=>{
+      const length=users.length;
+      users.splice(0,length);
+      return {
+        count:length,
+      };
+    },
+    //Event
+    addEvent:(parent,{data})=>{
+      const event={
+        id:nanoid(),
+        ...data
+      }
+      events.push(event);
+      return event;
+    },
+   
+    updateEvent:(parent,{id,data})=>{
+      const numId=parseInt(id);
+       console.log(numId)
+      const event_index=events.findIndex((event)=> event.id === numId);
+      console.log(event_index)
+      if(event_index === -1){
+        throw new Error("Event not found");
+      }
+      const updated_event=events[event_index]={
+          ...events[event_index],
+          ...data,
+      }
+      return updated_event;
+
+    },
+    deleteEvent:(parent,{id})=>{
+      const numId=parseInt(id);
+      const event_index=events.findIndex((event)=>event.id===numId);
+      if(event_index === -1){
+        throw new Error("Event not found"); 
+      }
+      const deleted_event=events[event_index];
+      events.splice(event_index, 1);
+      return deleted_event;
+    },
+    deleteAllEvents:()=>{
+      const length=events.length;
+      events.splice(0,length);
+      return {
+        count:length,
+      };
+    },
+  
+    //Location
+    addLocation:(parent,{data})=>{
+      const location={
+        id: nanoid(),
+        ...data
+      }
+      locations.push(location);
+      return location;
+    },
+    updateLocation:(parent,{id,data})=>{
+      const numId=parseInt(id);
+
+      const location_index=locations.findIndex((location)=>location.id===numId);
+      if(location_index===-1){
+        throw new Error("Location not found")
+      }
+      const updated_location=locations[location_index]={
+        ...locations[location_index],
+        ...data,
+      }
+      return updated_location;
+    },
+    deleteLocation:(parent,{id})=>{
+      const numId=parseInt(id);
+      const location_index=locations.findIndex((location)=>location.id===numId);
+      if(location_index===-1){
+        throw new Error("Location not found");
+      }
+      const deleted_location=locations[location_index];
+      locations.splice(location_index, 1);
+      return deleted_location;
+    },
+    deleteAllLocations:()=>{
+      const length=locations.length;
+      locations.splice(0,length);
+      return {
+        count:length,
+      };
+    },
+
+    //Participant
+    addParticipant:(parent,{data})=>{
+      const participant={
+        id:nanoid(),
+        ...data
+      }
+      participants.push(participant);
+      return participant;
+    },
+    updateParticipant:(parent,{id,data})=>{
+      const numId=parseInt(id);
+
+      const participant_index=participants.findIndex((participant)=>participant.id===numId);
+      if(participant_index===-1){
+        throw new Error("Participant not found");
+      }
+      const updated_participant=participants[participant_index]={
+        ...participants[participant_index],
+        ...data,
+      }
+      return updated_participant;
+    },
+    deleteParticipant:(parent,{id})=>{
+      const numId=parseInt(id);
+      const participant_index=participants.findIndex((participant)=>participant.id===numId);
+      if(participant_index===-1){
+        throw new Error("Participant not found");
+      }
+      const deleted_participant=participants[participant_index];
+      participants.splice(participant_index, 1);
+      return deleted_participant;
+    },
+    deleteAllParticipants:()=>{
+      const length=participants.length;
+      participants.splice(0,length);
+      return {
+        count:length,
+      };
+    },
+  },
   Query: {
     //User
     users: () => users,
@@ -87,7 +339,9 @@ const resolvers = {
       participants.find((participant) => participant.id === parseInt(args.id)),
   },
   Event: {
-    user: (parent) => users.find((user) => user.id === parent.user_id),
+    user: (parent) => users.find((user) => user.id === parent.user_id),    
+    
+    
     location: (parent) =>
       locations.find((location) => location.id === parent.location_id),
     participants: (parent) =>
